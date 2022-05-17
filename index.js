10098e9bdc22472c55236fee889c9b2fc2460420
iqbalbaharum <balainkk@gmail.com>
2022-05-18 01:18:18 +0800

    update parsing using name rather than attribute.name

--- index.js
+++ index.js
@@ -2,15 +2,43 @@ var parsedJSON = require('./data')
 
 let counts = []
 
+let findings = [
+    'OG',
+    'Fast hands',
+    'SVA Bike',
+    'Hoverboard',
+    'Cloud'
+]
+
+let total = 0
+
 for(let data of parsedJSON) {
 
-    let obj = data.metadata.attributes.find(e => e.trait_type === 'Name')
-    if(obj) {
-        if(counts[obj.value] > 0)
-            counts[obj.value]++
-        else
-            counts[obj.value] = 1
+    let findHash = data.tokenData.name.indexOf('#')
+    if(findHash) {
+        let n = data.tokenData.name.substring(0, findHash)
+        if(n) {
+            if(counts[n] > 0)
+                counts[n]++
+            else
+                counts[n] = 1
+        }
+    } else {
+        console.log('invalid: ' + data.tokenData.name)
     }
+
+    total++
+
+    // let obj = data.metadata.attributes.find(e => e.trait_type === 'Name')
+    // if(obj) {
+    //     if(counts[obj.value] > 0)
+    //         counts[obj.value]++
+    //     else
+    //         counts[obj.value] = 1
+    // }
+
+
 }
 
 console.log(counts)
+console.log('TOTAL: ' + total)
